cd14a0799c1ce8442f4528f7bdcae3474ba243ca
sandeepgoudn <94834716+sandeepgoudn@users.noreply.github.com>
2021-11-23 18:01:48 +0530

    Update

--- demo-project/src/main/java/com/accenture/nace/advice/NaceControllerAdvice.java
+++ demo-project/src/main/java/com/accenture/nace/advice/NaceControllerAdvice.java
@@ -6,12 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.accenture.nace.exception.EmptyInputException;
 import com.accenture.nace.exception.NoResourceException;
 
 @ControllerAdvice
-public class NaceControllerAdvice {
+public class NaceControllerAdvice ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(EmptyInputException.class)
 	public ResponseEntity<String> handleEmptyInput(EmptyInputException emptyInputException){
